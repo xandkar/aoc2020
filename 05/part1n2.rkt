74e129abfabbd11b5302c174d0d278810bc15606
Siraaj Khandkar <siraaj@khandkar.net>
2020-12-06 16:36:29 -0500

    Merge parts 1 and 2

--- 05/part1n2.rkt
+++ 05/part1n2.rkt
@@ -44,11 +44,34 @@
                    (string->list s)))
        (file->lines filepath)))
 
+(define/contract (drop-while-adjacent nums)
+  (-> (not/c (empty? (listof natural-number/c)))
+      (listof natural-number/c))
+  (define (drop prev nums)
+    (cond [(null? nums)
+           '()]
+          [(> (abs (- prev (car nums))) 1)
+           nums]
+          [else
+            (drop (car nums) (cdr nums))]))
+  (drop (car nums) (cdr nums)))
+
 ; (: main (-> Void))
 (define/contract (main)
   (-> void?)
-  (define data (read "input.txt"))
-  (displayln (apply max (map codes->seat data)))
-  (newline))
+  (define occupied (map codes->seat (read "input.txt")))
+  (define possible
+    (flatten
+      (map (lambda (row) (define r (* 8 row)) (map (lambda (c) (+ r c))
+                                                   (range 8)))
+           (range 128))))
+  (define unoccupied
+    (set->list (set-subtract (list->set possible) (list->set occupied))))
+  (define/contract unoccupied-and-available
+    (property/c length (=/c 1))
+    (drop-while-adjacent (sort (drop-while-adjacent (sort unoccupied <)) >)))
+  (define my-seat (first unoccupied-and-available))
+  (printf "part 1: ~a\n" (apply max occupied))
+  (printf "part 2: ~a\n" my-seat))
 
 (main)
