2df072644d2c6e9fed79694510d258bf37e1459e
Siraaj Khandkar <siraaj@khandkar.net>
2020-12-06 17:17:58 -0500

    Implement day 6 part 1

---
+++ 06/main.rkt
@@ -0,0 +1,21 @@
+#! /usr/bin/env racket
+#lang racket
+
+(require math/base)
+
+(define (uniq xs)
+  (set->list (list->set xs)))
+
+(define/contract (read filepath)
+  (-> path-string?
+      (listof (listof (listof char?))))
+  (map (λ (s) (map string->list
+                   (string-split s "\n")))
+       (string-split (file->string filepath) "\n\n")))
+
+(define (main)
+  (define data (read "input.txt"))
+  (printf "part 1: ~a\n" (sum (map (λ (group) (length (uniq (flatten group))))
+                                   data))))
+
+(main)
