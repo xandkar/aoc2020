4a04d501988aaa88f83738b57b9bad3e084a1a45
Siraaj Khandkar <siraaj@khandkar.net>
2020-12-06 14:43:31 -0500

    If you like it - put a contract on it

--- 05/part1.rkt
+++ 05/part1.rkt
@@ -1,45 +1,52 @@
 #! /usr/bin/env racket
 #lang racket
 
-; lower? -> (or/c list? #f)
-(define (lower? code)
+(define/contract (lower? code)
+  (-> (symbols 'F 'B 'L 'R)
+      (or/c list? #f))
   (member code '(F L)))
 
-; upper? -> (or/c list? #f)
-(define (upper? code)
+(define/contract (upper? code)
+  (-> (symbols 'F 'B 'L 'R)
+      (or/c list? #f))
   (member code '(B R)))
 
 ; (: search (-> Integer Integer (Listof Symbol) Integer))
-(define (search lo hi codes)
+(define/contract (search lo hi codes)
+  (-> natural-number/c
+      natural-number/c
+      (listof (symbols 'F 'B 'L 'R))
+      natural-number/c)
   (if (null? codes)
       lo
       (let ([range (/ (- hi lo) 2)]
             [c     (car codes)]
             [codes (cdr codes)])
-           (cond [(lower? c) (search lo          (- hi range) codes)]
-                 [(upper? c) (search (+ lo range) hi          codes)]))))
+           (cond [(lower? c) (search lo           (- hi range) codes)]
+                 [(upper? c) (search (+ lo range) hi           codes)]))))
 
-; (: codes->seat (-> (Listof Symbol) Integer))
-(define (codes->seat codes)
-  (define/contract row-codes
-    (listof (symbols 'F 'B))
-    (take codes 7))
-  (define/contract col-codes
-    (listof (symbols 'L 'R))
-    (drop codes 7))
+; (: codes->seat (-> (Listof (U 'F 'B 'L 'R)) Integer))
+(define/contract (codes->seat codes)
+  (-> (listof (symbols 'F 'B 'L 'R))
+      natural-number/c)
+  (define/contract row-codes (listof (symbols 'F 'B)) (take codes 7))
+  (define/contract col-codes (listof (symbols 'L 'R)) (drop codes 7))
   (define row (search 0 128 row-codes))
   (define col (search 0   8 col-codes))
   (define seat (+ (* 8 row) col))
   seat)
 
-; (: read (-> Path-String (Listof (Listof Symbol))))
-(define (read filepath)
+; (: read (-> Path-String (Listof (Listof (U 'F 'B 'L 'R)))))
+(define/contract (read filepath)
+  (-> path-string?
+      (listof (listof (symbols 'F 'B 'L 'R))))
   (map (λ (s) (map (λ (c) (string->symbol (string c)))
                    (string->list s)))
        (file->lines filepath)))
 
 ; (: main (-> Void))
-(define (main)
+(define/contract (main)
+  (-> void?)
   (define data (read "input.txt"))
   (displayln (apply max (map codes->seat data)))
   (newline))
