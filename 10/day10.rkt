2b305443b2fc0bdbac3f6c51e9c4a86bc092f1eb
Siraaj Khandkar <siraaj@khandkar.net>
2020-12-10 14:40:11 -0500

    Implement day 10 part 2

--- 10/day10.rkt
+++ 10/day10.rkt
@@ -19,17 +19,38 @@
 (define (hist xs)
   (foldl (λ (x h) (hash-update h x add1 0)) (hash) xs))
 
+(define (iter f xs)
+  (unless (null? xs)
+          (void (f (car xs)))
+          (iter f (cdr xs))))
+
+(define (count-paths dag root)
+  (define count-child
+    (let ([known (make-hash)])
+         (λ (c) (hash-ref! known c (λ () (count-parent c))))))
+  (define (count-parent root)
+    (define children (dict-ref dag root))
+    (if (null? children)
+        1
+        (foldl (λ (c total) (+ total (count-child c))) 0 children)))
+  (count-parent root))
+
+(define (day10 filepath)
+  (define given (read filepath))
+  (define implied
+    (let ([min 0]
+          [max (+ 3 (apply max given))])
+         (sort (cons min (cons max given)) <)))
+  (define diffs (hash-values (hist (intervals implied))))
+  (printf "~a part-1 ~a\n" filepath (apply * diffs))
+  (define nexts
+    (map (λ (x) (filter (λ (y) (and (>= y x) (<= y (+ 3 x)))) implied)) implied))
+  (printf "~a part-2 ~a\n" filepath (count-paths nexts (first implied)))
+  (newline))
+
 (define (main)
-  (for-each (λ (filepath)
-               (define data (read filepath))
-               (define implied
-                 (let ([min 0]
-                       [max (+ 3 (apply max data))])
-                      (cons min (cons max data))))
-               (define diffs (hash-values (hist (intervals implied))))
-               (printf "~a part-1 ~a ~a\n" filepath (apply * diffs) diffs))
-            (list "example1.txt"
-                  "example2.txt"
-                  "input.txt")))
+  (for-each day10 (list "example1.txt"
+                        "example2.txt"
+                        "input.txt")))
 
 (main)
