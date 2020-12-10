55471ccdf272a789020382412f05cfce9230cf3d
Siraaj Khandkar <siraaj@khandkar.net>
2020-12-10 10:25:05 -0500

    Implement day 10 part 1

---
+++ 10/day10.rkt
@@ -0,0 +1,34 @@
+#! /usr/bin/env racket
+#lang racket
+
+(define (read filepath)
+  (map string->number (file->lines filepath)))
+
+(define (ordered-pairs xs cmp)
+  (define (pairs xs n)
+    (cond [(null? xs) '()]
+          [(= n 0) '()]
+          [else (cons (cons (first xs) (second xs))
+                      (pairs (cdr xs) (sub1 n)))]))
+  (pairs (sort xs cmp) (sub1 (length xs))))
+
+(define (intervals xs)
+  (map (λ (pair) (- (cdr pair) (car pair))) (ordered-pairs xs <)))
+
+(define (hist xs)
+  (foldl (λ (x h) (hash-update h x add1 0)) (hash) xs))
+
+(define (main)
+  (for-each (λ (filepath)
+               (define data (read filepath))
+               (define implied
+                 (let ([min 0]
+                       [max (+ 3 (apply max data))])
+                      (cons min (cons max data))))
+               (define diffs (hash-values (hist (intervals implied))))
+               (printf "~a part-1 ~a ~a\n" filepath (apply * diffs) diffs))
+            (list "example1.txt"
+                  "example2.txt"
+                  "input.txt")))
+
+(main)
