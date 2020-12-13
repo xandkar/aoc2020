0f22a5e1069c5371db9d4d0ee3c4cb601f7a02bd
Siraaj Khandkar <siraaj@khandkar.net>
2020-12-13 13:14:53 -0500

    Simplify state updates

--- 12/main.rkt
+++ 12/main.rkt
@@ -1,8 +1,6 @@
 #! /usr/bin/env racket
 #lang racket
 
-(struct state (f x y) #:transparent)
-
 (define abs-face-dir/c (or/c 'N 'S 'E 'W))
 (define rel-face-dir/c (or/c 'L 'R))
 (define move-dir/c 'F)
@@ -10,6 +8,7 @@
                      rel-face-dir/c
                      move-dir/c))
 (define instruction/c (cons/c code/c number?))
+(define state/c (list/c abs-face-dir/c number? number?))
 
 (define/contract (string->ins s)
   (-> string? instruction/c)
@@ -29,22 +28,31 @@
   (define op (match in ['R +] ['L -]))
   (num->dir (modulo (op (dir->num from) (/ by 90)) 4)))
 
-(define (move instruction s)
+(define (get-f s) (list-ref s 0))
+(define (get-x s) (list-ref s 1))
+(define (get-y s) (list-ref s 2))
+
+(define (set-f s val) (list-set s 0 val))
+(define (set-x s val) (list-set s 1 val))
+(define (set-y s val) (list-set s 2 val))
+
+(define/contract (move1 instruction s)
+  (-> instruction/c state/c state/c)
   (match instruction
-         [(cons 'N n) (struct-copy state s [y (+ (state-y s) n)])]
-         [(cons 'S n) (struct-copy state s [y (- (state-y s) n)])]
-         [(cons 'E n) (struct-copy state s [x (+ (state-x s) n)])]
-         [(cons 'W n) (struct-copy state s [x (- (state-x s) n)])]
-         [(cons 'L n) (struct-copy state s [f (turn (state-f s) 'L n)])]
-         [(cons 'R n) (struct-copy state s [f (turn (state-f s) 'R n)])]
-         [(cons 'F n) (match (state-f s)
-                             ['N (struct-copy state s [y (+ (state-y s) n)])]
-                             ['S (struct-copy state s [y (- (state-y s) n)])]
-                             ['E (struct-copy state s [x (+ (state-x s) n)])]
-                             ['W (struct-copy state s [x (- (state-x s) n)])])]))
+         [(cons 'N n) (set-y s (+ (get-y s) n))]
+         [(cons 'S n) (set-y s (- (get-y s) n))]
+         [(cons 'E n) (set-x s (+ (get-x s) n))]
+         [(cons 'W n) (set-x s (- (get-x s) n))]
+         [(cons 'L n) (set-f s (turn (get-f s) 'L n))]
+         [(cons 'R n) (set-f s (turn (get-f s) 'R n))]
+         [(cons 'F n) (match (get-f s)
+                             ['N (set-y s (+ (get-y s) n))]
+                             ['S (set-y s (- (get-y s) n))]
+                             ['E (set-x s (+ (get-x s) n))]
+                             ['W (set-x s (- (get-x s) n))])]))
 
 (define (manhattan s)
-  (match s [(state _ x y) (+ (abs x) (abs y))]))
+  (+ (abs (get-x s)) (abs (get-y s))))
 
 (define (main)
   (for-each
@@ -52,7 +60,7 @@
        (define data (read input-filepath))
        (printf "~a part-1 ~a\n"
                input-filepath
-               (manhattan (foldl move (state 'E 0 0) data))))
+               (manhattan (foldl move1 '(E 0 0) data))))
     (list "example.txt"
           "input.txt")))
 
